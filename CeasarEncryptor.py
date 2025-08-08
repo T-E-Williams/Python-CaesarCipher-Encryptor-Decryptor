048b6059b07acf7813a4088c197b2a4a9e6f8792
Thomas Williams <tomevanwilliams@gmail.com>
2025-08-09 00:42:13 +0100

    Encryption by shift ammount finished (No Looping)

--- CeasarEncryptor.py
+++ CeasarEncryptor.py
@@ -1,1 +1,17 @@
-print("Hello World")
+
+def caesar_decrypt (ciphertext, shift):
+
+    decrypted = ""
+    
+    for char in ciphertext:
+
+        decrypted += chr(ord(char)-shift)
+
+
+    print(decrypted)
+    
+
+
+encrypted_string = "dbo"
+
+caesar_decrypt(encrypted_string, 3)
