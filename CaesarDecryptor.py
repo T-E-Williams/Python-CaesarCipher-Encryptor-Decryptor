67835034dd634b96d3891a088dbc892a48ab04d0
Thomas Williams <tomevanwilliams@gmail.com>
2025-08-09 22:49:21 +0100

    Changed Encryption unicode logic

---
+++ CaesarDecryptor.py
@@ -0,0 +1,5 @@
+print(97%26)
+print(chr((97%26)+78))
+
+for a in range (97,124):
+    print(chr((a%26)+78))
--- CaesarEncryptor.py
+++ CaesarEncryptor.py
@@ -4,14 +4,11 @@ def caesar_decrypt (ciphertext, shift):
     decrypted = ""
     
     for char in ciphertext:
-
-        decrypted += chr(ord(char)-shift)
-
+        
+        decrypted += chr(96+((ord(char)-shift)%26))
 
     print(decrypted)
     
-
-
 encrypted_string = "dbo"
 
-caesar_decrypt(encrypted_string, 3)
+caesar_decrypt(encrypted_string, -1)
