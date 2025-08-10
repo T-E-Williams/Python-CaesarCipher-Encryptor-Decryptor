407fdf3ebec2355fdb9dbca81175dde466439053
Thomas Williams <tomevanwilliams@gmail.com>
2025-08-10 20:51:57 +0100

    Added CipherText and a txt file reader

--- CaesarDecryptor.py
+++ CaesarDecryptor.py
@@ -1,16 +1,45 @@
+from wordfreq import zipf_frequency
+
+def real_word_checker(word):
+    return zipf_frequency(word.lower(), 'en') > 0
+        
 def caesar_decrypt(ciphertext, shift):
     decrypted = ""
-    shift = shift % 26  # normalize shift to 0–25
+    shift = shift % 26  
 
     for char in ciphertext:
         if char.isalpha():
             base = ord('a') if char.islower() else ord('A')
-            # subtract shift for decryption
+            
             decrypted += chr((ord(char) - base - shift) % 26 + base)
         else:
-            decrypted += char  # keep non-letters as-is
+            decrypted += char
+    print(real_word_checker(decrypted)) 
     return decrypted
 
+
+def read_file(file_name):
+    lines_as_words = []  
+
+    with open(file_name, "r") as file:
+        for line in file:
+            words = line.strip().split()  
+            lines_as_words.append(words) 
+
+    return lines_as_words
+
+        
+
+
 encrypted_string = "dbo"
-print(caesar_decrypt(encrypted_string, 1))   # -> "can"
-print(caesar_decrypt(encrypted_string, 52))  # -> "dbo"
+print(caesar_decrypt(encrypted_string, 2))
+
+
+#result = read_file("CipherText.txt")
+#print(result)
+
+
+
+
+
+
